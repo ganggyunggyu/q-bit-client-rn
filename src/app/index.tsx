5bd8c62bddd79a1184575d800d0bb006e5da12f0
ganggyunggyu <qwzx16@naver.com>
2025-07-18 09:36:00 +0900

    feat: add color area

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,53 @@
+import { BACKGROUND_COLOR, PRIMARY_COLOR, WHITE } from '@/styles/color';
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { WebView } from 'react-native-webview';
 
 export default function Index() {
+  const [webviewPath, setWebviewPath] = React.useState('');
+
+  const getTopBg = () => {
+    if (webviewPath === '/start') return PRIMARY_COLOR;
+
+    return BACKGROUND_COLOR;
+  };
+
+  const getBottomBg = () => {
+    if (webviewPath === '/start') return PRIMARY_COLOR;
+
+    if (webviewPath.includes('onboarding')) return BACKGROUND_COLOR;
+
+    return WHITE;
+  };
+  const topBackgroundColor = getTopBg();
+  const bottomBackgroundColor = getBottomBg();
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
+      <SafeAreaView
+        edges={['top']}
+        style={{ backgroundColor: topBackgroundColor }}
+      />
       <WebView
+        style={styles.webView}
         source={{ uri: 'https://q-bit.shop' }}
         startInLoadingState
         javaScriptEnabled
         domStorageEnabled
       />
-    </SafeAreaView>
+      <SafeAreaView
+        edges={['bottom']}
+        style={{ backgroundColor: bottomBackgroundColor }}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  webView: {
+    flex: 1,
   },
 });
---
+++ src/styles/color.ts
@@ -0,0 +1,22 @@
+// 라스트리 색상
+
+export const PRIMARY_COLOR = '#ba001c';
+export const PRIMARY_LIGHT_COLOR = '#eba9ad';
+
+export const SUB_COLOR = '#09d5e6';
+export const BACKGROUND_COLOR = '#f3efee';
+
+export const TEXT_COLOR = '#000000';
+export const SUBTEXT_COLOR = '#6B7280';
+export const BORDER_COLOR = '#D1D5DB';
+export const DISABLED_COLOR = 'rgba(0, 0, 0, 0.3)';
+
+export const WHITE = '#fdfdfd';
+
+// 카카오 색상
+
+export const KAKAO_YELLOW = '#FEE500';
+export const KAKAO_BLACK = '#181600';
+export const KAKAO_TEXT = '#3C1E1E';
+export const KAKAO_BROWN_LIGHT = '#CBA46C';
+export const KAKAO_BROWN_DARK = '#381E1F';
